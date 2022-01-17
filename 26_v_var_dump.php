b91161ebe6ee690483204ca71c665cd73b59bb52
Wilson <wiston.98@hotmail.com>
2022-01-16 20:57:47 -0500

    algunos apuntes

---
+++ 26_v_var_dump.php
@@ -0,0 +1,21 @@
+<?php 
+
+$arreglo = ['wilson', 'leonardo', 'miguel'];
+
+foreach($arreglo as $item){
+  echo "<h2> $item </h2>";
+  echo "<br>";
+}
+
+
+echo "<br>";
+
+
+var_dump($arreglo);
+
+//var_dump()->arroja o muestra en pantalla lo que tiene la variable
+echo "<br>";
+$edad = 20;
+var_dump($edad)
+
+?>
