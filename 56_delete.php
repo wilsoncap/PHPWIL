9d16c55065fb64874e97d60c332228d891f0912e
Wilson <wiston.98@hotmail.com>
2022-01-27 22:55:39 -0500

    delete con php

---
+++ 56_delete.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $mysql = new mysqli("db4free.net", "wilsonpru270921", "wili@159", "prueba270921");
+  if ($mysql->connect_error)
+    die("Problemas con la conexión a la base de datos");
+
+  $registro = $mysql->query("select descripcion from articulos where codigo=$_REQUEST[codigo]") or
+    die($mysql->error);
+
+  if ($reg = $registro->fetch_array()) {
+    $mysql->query("delete from articulos where codigo=$_REQUEST[codigo]") or
+      die($mysql->error);
+    echo 'La descripción del artículo que se eliminó es:' . $reg['descripcion'];
+  } else
+    echo 'No existe un artículo con dicho código';
+
+  $mysql->close();
+
+  ?>
+</body>
+</html>
